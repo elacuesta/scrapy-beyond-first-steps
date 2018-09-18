97bbf16c4be6dab833b545d588bfb1eac34b473c
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2018-09-18 14:57:58 -0300

    Simplify Books spiders

--- pybr2018/spiders/books.py
+++ pybr2018/spiders/books.py
@@ -2,9 +2,6 @@
 import scrapy
 
 
-numeric_ratings = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
-
-
 class BooksSpider(scrapy.Spider):
     """
     Base spider with common methods to extract links and book data
@@ -17,17 +14,16 @@ class BooksSpider(scrapy.Spider):
 
     def parse(self, response):
         for book_url in self.extract_book_urls(response):
-            self.logger.info('Scheduling request for %s', book_url)
+            self.logger.info('Scheduling: %s', book_url)
             yield scrapy.Request(book_url, callback=self.parse_book)
 
     def parse_book(self, response):
-        self.logger.info('Extracting book from %s', response.url)
-        rating = response.css('p.star-rating::attr(class)').get('').split(' ')[-1]
+        self.logger.info('Extracting: %s', response.url)
         return {
             'url': response.url,
             'title': response.css('h1::text').get(),
             'price': response.css('p.price_color::text').get(),
-            'rating': numeric_ratings.get(rating.lower()),
+            'rating': response.css('p.star-rating::attr(class)').get('').split()[-1],
         }
 
 
@@ -37,7 +33,7 @@ class SequentialBooksSpider(BooksSpider):
     """
     name = 'books-sequential'
 
-    pending_book_urls = set()
+    pending_book_urls = []
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
@@ -48,10 +44,9 @@ class SequentialBooksSpider(BooksSpider):
     def schedule_request(self):
         if self.pending_book_urls:
             book_url = self.pending_book_urls.pop()
-            self.logger.info('Scheduling request for %s', book_url)
+            self.logger.info('Scheduling: %s', book_url)
             request = scrapy.Request(book_url, callback=self.parse_book)
             self.crawler.engine.crawl(request, self)
 
     def parse(self, response):
-        for link in self.extract_book_urls(response):
-            self.pending_book_urls.add(link)
+        self.pending_book_urls.extend(self.extract_book_urls(response))
